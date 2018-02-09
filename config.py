2466ca08126d64d3519da76f0c05a9a3b4fe7c5d
Zach Chan <web_zach@foxmail.com>
2018-02-09 21:55:21 +0800

    up url

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """Configures for KindleEar, the First two variable is must to modify.
 KindleEar配置文件，请务必修改开始两个配置（如果使用uploader，则uploader自动帮你修改）
 """
 
-SRC_EMAIL = "akindleear@gmail.com"  #Your gmail account for sending mail to Kindle
-DOMAIN = "http://kindleear.appspot.com/" #Your domain of app
+SRC_EMAIL = "zachchankindle@gmail.com"  #Your gmail account for sending mail to Kindle
+DOMAIN = "http://zachchanbook.appspot.com/" #Your domain of app
 
 TIMEZONE = 8  #Default timezone, you can modify it in webpage after deployed
 
@@ -13,8 +13,8 @@ DEFAULT_MASTHEAD = "mh_default.gif" #default masthead
 DEFAULT_COVER = "cv_default.jpg" #default cover, leave it empty will not add cover to book
 DEFAULT_COVER_BV = DEFAULT_COVER #default cover for merged-book, None indicates paste all covers into one, =DEFAULT_COVER enable the using of uploaded image.
 
-MY_FEEDS_TITLE = u'KindleEar'
-MY_FEEDS_DESC = u'RSS delivering from KindleEar'
+MY_FEEDS_TITLE = u'zachchanbook'
+MY_FEEDS_DESC = u'RSS delivering from zachchanbook'
 
 #default timeout for network connection
 CONNECTION_TIMEOUT = 60
